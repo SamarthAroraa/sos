a2823d89da0628df7bd8793cbe4d76b883cfb722
Samarth Arora <samarth.arora@samarth-ltmy8gc.internal.salesforce.com>
2022-06-28 19:54:54 +0530

    made routes

--- app.py
+++ app.py
@@ -3,10 +3,21 @@ import os
 from flask import Flask
 import requests
 from flask import request
+import base64
+def base64_to_wav(base64_string, output_wav_file):
+  wav_file = open(output_wav_file, "wb")
+  decode_string = base64.b64decode(base64_string)
+  wav_file.write(decode_string)
+
 # import torchaudio
 # from speechbrain.pretrained import EncoderClassifier
 
 import speechbrain as sb
+
+def base64_to_wav(base64_string, output_wav_file):
+  wav_file = open(output_wav_file, "wb")
+  decode_string = base64.b64decode(base64_string)
+  wav_file.write(decode_string)
 
 
 def send_slack_message(text):
@@ -45,11 +56,23 @@ def sendText():
     except:
         return {"message": "Internal server error", "status": 500}
 
-@app.route('/serveModel')
+@app.route('/serveModel', methods=['GET', 'POST'])
 def serveModel():
-    # return "Test"
-    classifier = sb.pretrained.EncoderClassifier.from_hparams(source="speechbrain/urbansound8k_ecapa", savedir="pretrained_models/gurbansound8k_ecapa")
-    example_audio_file_path = 'siren-1-decoded.wav'
-    out_prob, score, index, text_lab = classifier.classify_file(example_audio_file_path)
-    print(text_lab)
-    return(str(text_lab[0]))
+    if(request.method == 'GET'):
+        return 'This is a post request test, use post method'
+    if(request.method == 'POST'):
+        file = request.files['wav_file']
+        file.save('data/runtime.wav')
+        
+        # return "Test"
+        classifier = sb.pretrained.EncoderClassifier.from_hparams(source="speechbrain/urbansound8k_ecapa", savedir="pretrained_models/gurbansound8k_ecapa")
+        example_audio_file_path = 'data/runtime.wav'
+        out_prob, score, index, text_lab = classifier.classify_file(example_audio_file_path)
+        print(text_lab)
+        return(str(text_lab[0]))
+
+@app.route('/getBlob', methods=['GET', 'POST'])
+def convertFileToBlob():
+    file = request.files['wav_file']
+    encode_string = base64.b64encode(file.read())
+    return encode_string
