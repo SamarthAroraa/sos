a6930fda88204e7956899cd094e35d411db66644
Samarth Arora <samarth.arora@samarth-ltmy8gc.internal.salesforce.com>
2022-06-29 16:02:31 +0530

    notof changes

--- client-app/App.tsx
+++ client-app/App.tsx
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import * as React from 'react';
+import { useEffect } from 'react';
+import notifee from '@notifee/react-native';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
@@ -8,6 +11,37 @@ import Navigation from './navigation';
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+
+  async function onDisplayNotification() {
+    console.log('notif sendind')
+    // Request permissions (required for iOS)
+    await notifee.requestPermission()
+
+    // Create a channel (required for Android)
+    const channelId = await notifee.createChannel({
+      id: 'default',
+      name: 'Default Channel',
+    });
+
+    // Display a notification
+    await notifee.displayNotification({
+      title: 'Notification Title',
+      body: 'Main body content of the notification',
+      android: {
+        channelId,
+        // smallIcon: 'name-of-a-small-icon', // optional, defaults to 'ic_launcher'.
+        // pressAction is needed if you want the notification to open the app when pressed
+        pressAction: {
+          id: 'default',
+        },
+      },
+    });
+  }
+  useEffect(() => {
+    setInterval(() => onDisplayNotification(), 1000)
+  }, [])
+
 
   if (!isLoadingComplete) {
     return null;
